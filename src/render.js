14fff8c7845cd7e67c21b83a69050575bc490c60
Alg3rn0n <alg3rn0n@Alg3rn0n.Bmo>
2026-02-13 17:01:34 +0100

    template finiti, inizio render.js

--- src/render.js
+++ src/render.js
@@ -5,10 +5,20 @@ const hookpage = document.querySelector("body"); // hook della pagina
  * idea:
  *      titolo----save
  *      aside----main
- *        |     |  lista degli ordini con bottone +,-, remove  
+ *        |     |        lista degli ordini con bottone +,-, remove  
  *      ordini---
  *        |     |
  *      Vecchi ordini                         btn-sort
  * 
  * quando clicci su remove dialog con sei sicuro, se si rimuove altrimenti torna indietro.
  */
+
+
+/* seleziono i vari template da manipolare sul file js*/
+const paginaTemplate = document.querySelector("#pag");
+const dialogTemplate = document.querySelector("#dialog-template");
+const orderCardTemplate = document.querySelector(".card");
+
+/* ora per manipolare un nodo si fa: const node = document.importNode(template.content, true);*/
+
+
