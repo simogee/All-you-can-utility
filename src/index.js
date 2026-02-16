d4fa7fcc9b7fa6f115b75e6913f8c20e6ad2bffe
Alg3rn0n <alg3rn0n@Alg3rn0n.Bmo>
2026-02-16 17:55:12 +0100

    ops

--- src/index.js
+++ src/index.js
@@ -19,18 +19,6 @@ import{AddOrder,ReduceOrder,getOrders,getOldOrders,DeleteOrder,SortingOrders} fr
 
 const parts = RenderShell();
 
-
-// function testOrder(n){
-//     for(let i = 0; i < n; i++){
-//         AddOrder("prova",123);
-//         AddOrder("gigi",222);
-//     }
-//         console.log(getOrders());
-
-// }
-
-
-// testOrder(10);
 const dialog = RenderDialog(document.querySelector("body"));
 
 
